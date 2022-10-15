da121a77e174058862b6704c1937fa7c7436fa5d
Alina <alilaursdesign@gmail.com>
2022-10-15 17:23:25 +0200

    Changes in contiguous

--- index.js
+++ index.js
@@ -8,12 +8,57 @@ const board = [
     [false, false, false, false, false, false, false, false, false, false],
     [false, false, false, false, false, false, false, false, false, false],
     [false, false, false, false, false, false, false, false, false, false],
-    [true, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false],
 ];
 
-let count = 0;
-
 const contiguous = (y, x) => {
+    if ((y = 0) && (x = 0)) {
+        if (board[y][x + 1]) {
+            count++;
+        }
+        if (board[y + 1][x]) {
+            count++;
+        }
+        if (board[y + 1][x + 1]) {
+            count++;
+        }
+    }
+
+    if ((y = 0) && (x = 0)) {
+        if (board[y][x - 1]) {
+            count++;
+        }
+        if (board[y + 1][x - 1]) {
+            count++;
+        }
+        if (board[y + 1][x]) {
+            count++;
+        }
+    }
+
+    if ((y = 0) && (x = 0)) {
+        if (board[y - 1][x]) {
+            count++;
+        }
+        if (board[y - 1][x + 1]) {
+            count++;
+        }
+        if (board[y][x + 1]) {
+            count++;
+        }
+    }
+
+    if ((y = 0) && (x = 0)) {
+        if (board[y - 1][x]) {
+            count++;
+        }
+        if (board[y - 1][x - 1]) {
+            count++;
+        }
+        if (board[y][x - 1]) {
+            count++;
+        }
+    }
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             let count = 0;
